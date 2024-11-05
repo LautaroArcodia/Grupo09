cafefbcc7988724f611171695fc4fbccb1ce52cf
LautaroArcodia <lautaroarcodia@gmail.com>
2024-11-05 12:32:30 -0300

    Elimina path local

--- 02_EjecucionSP.sql
+++ 02_EjecucionSP.sql
@@ -3,15 +3,15 @@ USE Com5600G09;
 BEGIN TRY
 	BEGIN TRANSACTION
 
-	 EXEC ddbba.InsertarSucursales @Ruta = 'C:\Users\Lautaro\Desktop\UNLAM\BD_Aplicadas\TP\TP_integrador_Archivos\Informacion_complementaria.xlsx';
+	 EXEC ddbba.InsertarSucursales @Ruta = 'Informacion_complementaria.xlsx';
 	
-	 EXEC ddbba.InsertarEmpleados @RutaArchivo = 'C:\Users\Lautaro\Desktop\UNLAM\BD_Aplicadas\TP\TP_integrador_Archivos\Informacion_complementaria.xlsx';
+	 EXEC ddbba.InsertarEmpleados @RutaArchivo = 'Informacion_complementaria.xlsx';
 
-	 EXEC ddbba.InsertarCatalogoEnProductos @RutaArchivo = 'C:\Users\Lautaro\Desktop\UNLAM\BD_Aplicadas\TP\TP_integrador_Archivos\Productos\catalogo.csv';
+	 EXEC ddbba.InsertarCatalogoEnProductos @RutaArchivo = 'catalogo.csv';
 
-	 EXEC ddbba.InsertarElectronicosEnProductos @RutaArchivo = 'C:\Users\Lautaro\Desktop\UNLAM\BD_Aplicadas\TP\TP_integrador_Archivos\Productos\Electronic accessories.xlsx';
+	 EXEC ddbba.InsertarElectronicosEnProductos @RutaArchivo = 'Electronic accessories.xlsx';
 
-	 EXEC ddbba.InsertarImportadosEnProductos @RutaArchivo = 'C:\Users\Lautaro\Desktop\UNLAM\BD_Aplicadas\TP\TP_integrador_Archivos\Productos\Productos_importados.xlsx';
+	 EXEC ddbba.InsertarImportadosEnProductos @RutaArchivo = 'Productos_importados.xlsx';
 	
 	COMMIT TRANSACTION
 END TRY
